f39ea00e9544bf1b4f95c52156a32eaf4f19c20c
Ojas Kavathe <ojaskavathe@gmail.com>
2022-08-20 19:16:04 +0530

    Changed port

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	http.Handle("/", fs)
 
 	//http.HandleFunc("/", HomeEndPoint)
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
