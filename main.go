4f04d510a7938e756864ebcc921f8efda393afd7
Ojas Kavathe <ojaskavathe@gmail.com>
2022-09-20 00:45:27 +0530

    Added Buttons and a Title

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 )
 
 func HomeEndPoint(w http.ResponseWriter, r *http.Request) {
@@ -11,13 +10,13 @@ func HomeEndPoint(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	port := os.Getenv("PORT")
+	//port := os.Getenv("PORT")
 	//for loading static assets
 	fs := http.FileServer(http.Dir("./web"))
 	http.Handle("/", fs)
 
 	//http.HandleFunc("/", HomeEndPoint)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":8080" /*+port*/, nil); err != nil {
 		log.Fatal(err)
 	}
 }
--- web/static/test.js
+++ web/static/test.js
@@ -4,6 +4,62 @@
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
+BABYLON.ArcRotateCamera.prototype.moveTo = function (whichprop, targetval, speed) {
+    var ease = new BABYLON.CubicEase();
+    ease.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+	BABYLON.Animation.CreateAndStartAnimation('at4', this, whichprop, speed, 360, this[whichprop], targetval, 0, ease);
+}
+
+var particleSystem;
+
+smoke = function (particleSystem) {
+    
+    // lifetime
+    particleSystem.minLifeTime = 2;
+    particleSystem.maxLifeTime = 6;
+
+    // emit rate
+    particleSystem.emitRate = 100;
+
+    // gravity
+    particleSystem.gravity = new BABYLON.Vector3(0, 1.5, 1.25);
+
+    // size gradient
+    particleSystem.addSizeGradient(0, 0.6, 1);
+    particleSystem.addSizeGradient(0.3, 1, 2);
+    particleSystem.addSizeGradient(0.5, 2, 3);
+    particleSystem.addSizeGradient(1.0, 6, 8);
+
+    // color gradient
+    particleSystem.addColorGradient(0, new BABYLON.Color4(0.5, 0.5, 0.5, 0),  new BABYLON.Color4(0.8, 0.8, 0.8, 0));
+    particleSystem.addColorGradient(0.4, new BABYLON.Color4(0.1, 0.1, 0.1, 0.1), new BABYLON.Color4(0.4, 0.4, 0.4, 0.4));
+    particleSystem.addColorGradient(0.7, new BABYLON.Color4(0.03, 0.03, 0.03, 0.2), new BABYLON.Color4(0.3, 0.3, 0.3, 0.4));
+    particleSystem.addColorGradient(1.0, new BABYLON.Color4(0.0, 0.0, 0.0, 0), new BABYLON.Color4(0.03, 0.03, 0.03, 0));
+
+    // speed gradient
+    particleSystem.addVelocityGradient(0, 1, 1.5);
+    particleSystem.addVelocityGradient(0.1, 0.8, 0.9);
+    particleSystem.addVelocityGradient(0.7, 0.4, 0.5);
+    particleSystem.addVelocityGradient(1, 0.1, 0.2);
+
+    // rotation
+    particleSystem.minInitialRotation = 0;
+    particleSystem.maxInitialRotation = Math.PI;
+    particleSystem.minAngularSpeed = -1;
+    particleSystem.maxAngularSpeed = 1;
+
+    // blendmode
+    particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_STANDARD;
+    
+    // emitter shape
+    var sphereEmitter = particleSystem.createSphereEmitter(0.1);
+
+    // Where the particles come from
+    particleSystem.emitter = new BABYLON.Vector3(7, 0, 4); // the starting object, the emitter
+    particleSystem.minEmitBox = new BABYLON.Vector3(-0.5, -0.5, -0.5); // Starting all from
+    particleSystem.maxEmitBox = new BABYLON.Vector3(0.5, 0.5, 0.5); // To...
+}
+
 const createScene = function () {
     const scene = new BABYLON.Scene(engine);
 
@@ -17,8 +73,51 @@ const createScene = function () {
     scene.animationTimeScale = 10;
 
     const camera = new BABYLON.ArcRotateCamera("camera", Math.PI, Math.PI / 2.5, 60, new BABYLON.Vector3(0, 3, -5));
+    camera.upperRadiusLimit = 200;
+    camera.lowerRadiusLimit = 30;
+    //camera.zoomToMouseLocation = true;
+
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+    // Create a particle system
+    particleSystem = new BABYLON.ParticleSystem("particles", 8000, scene);
+    //Texture of each particle
+    particleSystem.particleTexture = new BABYLON.Texture("https://raw.githubusercontent.com/PatrickRyanMS/BabylonJStextures/master/FFV/smokeParticleTexture.png", scene);
+    smoke(particleSystem);
+
+    // GUI
+    var plane = BABYLON.MeshBuilder.CreatePlane("plane", {size: 10});
+    //plane.parent = sphere;
+    plane.position.x = 10;
+    //plane.rotate(BABYLON.Vector3(0, 1, 0), Math.PI/2)
+
+    plane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+
+    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
+    var b_exhaust = BABYLON.GUI.Button.CreateSimpleButton("but1", "Exhaust");
+    b_exhaust.width = 0.4;
+    b_exhaust.height = 0.2;    ;
+    b_exhaust.color = "white";
+    b_exhaust.fontSize = 100;
+    b_exhaust.background = "blue";
+    b_exhaust.alpha = 0.5;
+    b_exhaust.onPointerUpObservable.add(function() {
+        camera.detachControl();
+        
+        //b_exhaust.textBlock.text = camera.target;
+        
+        camera.moveTo("target", new BABYLON.Vector3(0, 3, -5), 50);
+        camera.moveTo("position", new BABYLON.Vector3(14, 2, 18), 20);
+
+        particleSystem.start();
+        
+        //stop the smoke after 5s
+        setTimeout(() => { particleSystem.stop(); }, 5000);
+
+        camera.attachControl(canvas, true);
+    });
+    advancedTexture.addControl(b_exhaust);
 
     return scene;
 };
